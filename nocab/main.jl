2e5c24fece8dc467b051c135329dea6248c574e1
Arthur Bacon <bacon247@gmail.com>
2024-01-31 11:15:22 -0500

    Nocab clean up my workspace

--- nocab/main.jl
+++
@@ -1,9 +0,0 @@
-include("FileReaders/FileReader.jl")
-
-function main()
-    Nocab.readProcessWrite("nocab.txt", "output.txt")
-    # result = Nocab.getAllTextInFile("nocab.txt")
-    # print(result)
-end #function main()
-
-main()
