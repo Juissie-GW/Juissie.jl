e9dd715058bb81b67e8b72b34db92b7f75e01ba2
lucasmccabe <lucashmccabe@gmail.com>
2024-02-19 15:35:02 -0500

    feat: load_corpus

--- src/SemanticSearch/Backend.jl
+++ src/SemanticSearch/Backend.jl
@@ -6,14 +6,17 @@ using SQLite
 using HNSW
 using UUIDs
 using DataFrames
+using Serialization
+using JSON
 
 include("Embedding.jl")
 using .Embedding
 include("TextUtils.jl")
 using .TextUtils
 
+CURR_DIR = @__DIR__
 
-export Corpus, upsert_chunk, upsert_document, search
+export Corpus, upsert_chunk, upsert_document, search, load_corpus
 
 """
     struct Corpus
@@ -25,6 +28,10 @@ Basically a vector database. It will have these attributes:
 
 Attributes
 ----------
+corpus_name : String or Nothing
+    this is the name of your corpus and will be used to access saved 
+        corpuses
+    if Nothing, we can't save/load and everything will be in-memory
 db : a SQLite.DB connection object
     this is a real relational database to store metadata (e.g. chunk text, doc name)
 hnsw : Hierarchical Navigable Small World object
@@ -45,6 +52,7 @@ The struct is mutable because we want to be able to change things like
 incrementing next_idx.
 """
 mutable struct Corpus
+    corpus_name :: Union{String, Nothing}
     db :: SQLite.DB
     hnsw :: Union{HNSW.HierarchicalNSW, Nothing}
     embedder :: Embedding.Embedder
@@ -86,7 +94,12 @@ function Corpus(corpus_name::Union{String, Nothing}=nothing, embedder_model_path
     if isnothing(corpus_name)
         db = SQLite.DB() # in-memory db
     else
-        db = SQLite.DB("$(corpus_name).db")
+        db_path = "$(CURR_DIR)/files/$(corpus_name).db"
+        if isfile(db_path)
+            # file exists ; delete it
+            rm(db_path) 
+        end
+        db = SQLite.DB(db_path)
     end
 
     DBInterface.execute(db, "DROP TABLE IF EXISTS metadata")
@@ -100,7 +113,62 @@ function Corpus(corpus_name::Union{String, Nothing}=nothing, embedder_model_path
 
     # don't initialize hnsw yet because it needs initial features
     hnsw = nothing
-    return Corpus(db, hnsw, embedder, max_seq_len, [], 1)
+
+    # if not in-memory only, save info about the corpus as a json
+    if !isnothing(corpus_name)
+        corpus_data = Dict(
+            "embedder_model_path" => embedder_model_path,
+            "max_seq_len" => max_seq_len
+        )
+        json_str = JSON.json(corpus_data)
+        corpus_data_path = "$(CURR_DIR)/files/$(corpus_name)_data.json"
+        # test if the hnsw file exists
+        if isfile(corpus_data_path)
+            # file exists ; delete it
+            rm(corpus_data_path) 
+        end
+        open(corpus_data_path, "w") do file
+            write(file, json_str)
+        end
+    end
+
+    return Corpus(corpus_name, db, hnsw, embedder, max_seq_len, [], 1)
+end
+
+"""
+    function load_corpus(corpus_name)
+
+Loads an already-initialized corpus from its associated "artifacts" (relational
+database, vector index, and informational json).
+
+Parameters
+----------
+corpus_name : str
+    the name of your EXISTING vector database
+"""
+function load_corpus(corpus_name::String)
+    try
+        # sqlite database
+        db_path = "$(CURR_DIR)/files/$(corpus_name).db"
+        db = SQLite.DB(db_path)
+
+        # vector index
+        hnsw_path = "$(CURR_DIR)/files/$(corpus_name).bin"
+        hnsw = open(deserialize, hnsw_path)
+
+        # get corpus info from associated json
+        corpus_data_path = "$(CURR_DIR)/files/$(corpus_name)_data.json"
+        corpus_data = open(corpus_data_path, "r") do file
+            JSON.parse(read(file, String))
+        end
+        embedder_model_path = corpus_data["embedder_model_path"]
+        embedder = Embedder(embedder_model_path)
+        max_seq_len = corpus_data["max_seq_len"]
+    catch e
+        throw(ArgumentError("Loading failed; corpus name not found."))
+    end
+    
+    return Corpus(corpus_name, db, hnsw, embedder, max_seq_len, [], 1)
 end
 
 """
@@ -205,6 +273,23 @@ function index(corpus::Corpus)
             efConstruction=100, M=16, ef=50
     )
     add_to_graph!(corpus.hnsw)
+
+    if !isnothing(corpus.corpus_name)
+        # we have passed a name for the corpus, which means we're not
+        # doing in-memory only.
+        file_path = "$(CURR_DIR)/files/$(corpus.corpus_name).bin"
+
+        # test if the hnsw file exists
+        if isfile(file_path)
+            # file exists ; delete it
+            rm(file_path) 
+        end
+
+        # now serialize the vector index
+        open(file_path, "w") do file
+            serialize(file, corpus.hnsw)
+        end
+    end
 end
 
 """
