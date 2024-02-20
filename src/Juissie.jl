c31c1a3cf4c21d096105e8324bf208d32d529820
lucasmccabe <lucashmccabe@gmail.com>
2024-02-19 19:08:46 -0500

    feat: generate text with OAI

--- src/Juissie.jl
+++ src/Juissie.jl
@@ -1,9 +1,13 @@
 module Juissie
 
 include("SemanticSearch/SemanticSearch.jl")
+include("Generation.jl")
 
 using .SemanticSearch
 export Corpus, upsert_chunk, upsert_document, search,
-    Embedder, embed
+    Embedder, embed,
+
+using .Generation
+export OAIGenerator, generate
 
 end
