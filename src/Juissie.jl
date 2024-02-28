2579a5c26ea11069a91e5952a2a13a10e74cf832
lucasmccabe <lucashmccabe@gmail.com>
2024-02-27 22:21:49 -0500

    feat: generate with corpus 2: electric boogaloo

--- src/Juissie.jl
+++ src/Juissie.jl
@@ -4,10 +4,22 @@ include("SemanticSearch/SemanticSearch.jl")
 include("Generation.jl")
 
 using .SemanticSearch
-export Corpus, upsert_chunk, upsert_document, search,
-    Embedder, embed, load_corpus
+export Corpus, 
+    upsert_chunk, 
+    upsert_document, 
+    upsert_document_from_url,
+    search,
+    Embedder, 
+    embed,
+    load_corpus
 
 using .Generation
-export OAIGenerator, generate
+export OAIGenerator,
+    OAIGeneratorWithCorpus,
+    generate, 
+    generate_with_corpus,
+    upsert_chunk_to_generator,
+    upsert_document_to_generator,
+    upsert_document_from_url_to_generator
 
 end
